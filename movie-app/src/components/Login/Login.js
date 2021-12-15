d78fedcedaa97f0262797ab86739379c3212875a
cgkw94 <colingohh@gmail.com>
2021-12-15 14:56:56 +0800

    minor changes to sign up form

--- movie-app/src/components/Login/Login.js
+++ movie-app/src/components/Login/Login.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/Header";
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const [users, setUsers] = useState(JSON.parse(localStorage.getItem("user")));
+  console.log(users);
+
+  const onSubmit = () => {
+    console.log("login successful");
+  };
+
   return (
     <div>
       <Header />
@@ -12,6 +23,35 @@ const Login = () => {
         </Link>{" "}
         / Login
       </h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="username">
+            <p>Username</p>
+            <input
+              type="text"
+              id="username"
+              {...register("username", {
+                required: "Username Required!",
+              })}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="password">
+            <p>Password</p>
+            <input
+              type="text"
+              id="password"
+              {...register("password", {
+                required: "Password Required!",
+              })}
+            />
+          </label>
+        </div>
+        <Button value="Login" type="submit" />
+      </form>
     </div>
   );
 };
--- movie-app/src/components/SignUp/SignUp.js
+++ movie-app/src/components/SignUp/SignUp.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../Button/Button";
 import Header from "../Header/Header";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const SignUp = () => {
+  const history = useHistory();
+
   const { register, formState, handleSubmit } = useForm();
 
   const [userDetails, setUserDetails] = useState({
@@ -17,8 +19,14 @@ const SignUp = () => {
 
   const onSubmit = () => {
     setUser((prevState) => {
-      return [userDetails, ...prevState];
+      return [...prevState, userDetails];
     });
+
+    alert(
+      `Your account (${userDetails.username}) has been successfully created! Please check your email and confirm your registration `
+    );
+
+    history.push("/login");
     localStorage.setItem("user", JSON.stringify(user));
   };
 
@@ -33,70 +41,76 @@ const SignUp = () => {
       </h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <p style={{ color: "red" }}>
-            {formState.errors.username && formState.errors.username.message}
-          </p>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            {...register("username", {
-              onChange: (e) => {
-                setUserDetails((prevState) => {
-                  return { ...prevState, username: e.target.value };
-                });
-              },
-              required: "Username Required!",
-              minLength: {
-                message: "Username must be at least 5 characters!",
-                value: 5,
-              },
-            })}
-          />
+          <label htmlFor="username">
+            <p>Username</p>
+            <input
+              type="text"
+              id="username"
+              {...register("username", {
+                onChange: (e) => {
+                  setUserDetails((prevState) => {
+                    return { ...prevState, username: e.target.value };
+                  });
+                },
+                required: "Username Required!",
+                minLength: {
+                  message: "Username must be at least 5 characters!",
+                  value: 5,
+                },
+              })}
+            />
+            <p style={{ color: "red" }}>
+              {formState.errors.username && formState.errors.username.message}
+            </p>
+          </label>
         </div>
         <div>
-          <p style={{ color: "red" }}>
-            {formState.errors.email && formState.errors.email.message}
-          </p>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            {...register("email", {
-              onChange: (e) => {
-                setUserDetails((prevState) => {
-                  return { ...prevState, email: e.target.value };
-                });
-              },
-              required: "Email Required!",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: "Enter a valid e-mail address",
-              },
-            })}
-          />
+          <label htmlFor="email">
+            <p>Email</p>
+            <input
+              type="text"
+              id="email"
+              {...register("email", {
+                onChange: (e) => {
+                  setUserDetails((prevState) => {
+                    return { ...prevState, email: e.target.value };
+                  });
+                },
+                required: "Email Required!",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Enter a valid e-mail address",
+                },
+              })}
+            />
+            <p style={{ color: "red" }}>
+              {formState.errors.email && formState.errors.email.message}
+            </p>
+          </label>
         </div>
         <div>
-          <p style={{ color: "red" }}>
-            {formState.errors.password && formState.errors.password.message}
-          </p>
-          <label htmlFor="password">Password</label>
-          <input
-            type="text"
-            id="password"
-            {...register("password", {
-              onChange: (e) => {
-                setUserDetails((prevState) => {
-                  return { ...prevState, password: e.target.value };
-                });
-              },
-              required: "Password required!",
-              minLength: {
-                message: "Password must be at least 8 characters!",
-                value: 8,
-              },
-            })}
-          />
+          <label htmlFor="password">
+            <p>Password</p>
+            <input
+              type="text"
+              id="password"
+              {...register("password", {
+                onChange: (e) => {
+                  setUserDetails((prevState) => {
+                    return { ...prevState, password: e.target.value };
+                  });
+                },
+                required: "Password required!",
+                minLength: {
+                  message: "Password must be at least 8 characters!",
+                  value: 8,
+                },
+              })}
+            />
+            <p style={{ color: "red" }}>
+              {formState.errors.password && formState.errors.password.message}
+            </p>
+          </label>
         </div>
         <div>
           <Button value="Register" type="submit" />
